fd0b8f41b3ec32fe503ba227d63210c108e87cc9
emilapuente1 <132621086+emilapuente1@users.noreply.github.com>
2023-06-08 16:26:49 -0300

    Update
    Se modificaron las columnas, se agregó las columnas de diferencia de precio. AE38,AL41,AL30

--- Bondarg.py
+++ Bondarg.py
@@ -8,9 +8,17 @@ Created on Wed Jun  7 22:16:45 2023
 import openpyxl
 import pandas as pd
 
-Excelworkbook=openpyxl.load_workbook("H:\Documentos\Practica Pyhton Bond Arg\Dataset bonos arg usd.xlsx")
+Excelworkbook=openpyxl.load_workbook("D:\Documentos\GitHub\Practica-Pyhton-Bond-Arg\Dataset bonos arg usd.xlsx")
 Excelsheet=Excelworkbook.active
 
-bondarg=pd.DataFrame(Excelsheet.values)
+#obtener los titulos
+titulos= next(Excelsheet.values)[0:]
 
-print(bondarg)
+bondarg=pd.DataFrame(Excelsheet.values,columns=titulos)
+bondarg=bondarg.drop(bondarg.index[0])
+
+# creamos las columnas de diferencia de precio entre AE38-AL30 y Al41 - AL30
+bondarg["Diferencia Precio 38-30"]=bondarg[" AE38D "]-bondarg[" AL30D "]
+bondarg["Diferencia Precio 41-30"]=bondarg[" AL41D "]-bondarg[" AL30D "]
+
+
